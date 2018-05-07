3431079277309aff5e539ac60ceddd88787be64d
ArtemTszyu <32194510+ArtemTszyu@users.noreply.github.com>
2018-05-07 15:17:52 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
+template <typename T>
 class vector_t
 {
 private:
-	int * elements_;
+	T * elements_;
 	std::size_t size_;
 	std::size_t capacity_;
 public:
@@ -15,13 +16,20 @@ public:
 	std::size_t size() const;
 	std::size_t capacity() const;
 
-	void push_back(int value);
+	void push_back(T value);
 	void pop_back();
 
 	int & operator [](std::size_t index);
 	int operator [](std::size_t index) const;
 
 	bool operator ==(vector_t const & other) const;
+	
+	T & at(std::size_t index) {
+	if(index >= size_) {
+		throw std::out_of_range("Out of range");
+		}
+	return (*this)[index];
+	}
 };
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs);
